15d84e4c0dcc3a1e24b93a878cb2089ff2f4934a
Sakura Mo <moyinzi@outlook.com>
2019-06-02 01:23:13 +0800

    rewrite autoLoad

--- api/auth/handler/common.go
+++ api/auth/handler/common.go
@@ -1,14 +1,10 @@
 package handler
 
 import (
-	"context"
-
 	"github.com/ddosakura/starmap/api/common"
 	"github.com/ddosakura/starmap/api/rest"
 )
 
 var (
-	autoLoadAuthService = rest.LoadAuthService(func(ctx context.Context) (rest.AuthService, bool) {
-		return common.AuthUserFromContext(ctx)
-	})
+	autoLoadAuthService = rest.LoadAuthService(common.AuthServiceUserFromContext)
 )
